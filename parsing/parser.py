cd6b2655ab72dd1e2d31ae1aa1be66e12eb8e979
N <166038881+naama-80820@users.noreply.github.com>
2024-06-16 04:20:45 +0300

    Update parser.py

--- parsing/parser.py
+++ parsing/parser.py
@@ -1,200 +1,194 @@
 import ply.yacc as yacc
-from lexing import lexer
-from ast import *
+from lexing.lexer import lexer, tokens
+from semantic.my_ast import Num, BinOp, Print, Str, VarAssign, Var, If, Else, While, For, List
+from code_gen.code_generator import generate_code
 
-tokens = lexer.tokens
+precedence = (
+    ("right", "EQUAL"),
+    ('left', 'PLUS', 'MINUS', 'STAR', 'SLASH'),
+    ('left', 'GREATER', 'LESS', 'EQUAL_EQUAL', 'NOT_EQUAL', 'GREATER_EQUAL', 'LESS_EQUAL'),
+)
 
-# Parsing rules
+def p_statements(p):
+    '''
+    statements : statement
+               | statements statement
+    '''
+    if len(p) == 2:
+        p[0] = [p[1]]
+    else:
+        p[0] = p[1] + [p[2]]
 
-# Define other token rules (number, string, identifier)
-def t_NUMBER(t):
-    r"\d+"
-    t.value=int(t.value)
-    return t
-
-def t_STRING(t):
-    r'"([^"\\]|\\.)*"'
-    t.lexer.lineno+=t.value.count("\n")
-    return t
-
-def t_IDENTIFIER(t):
-    r"[a-zA-Z_א-ת][a-zA-Z_0-9_א-ת]*"
-    t.type = reserved.get(t.value, "IDENTIFIER")  # Check for reserved words
-    return t
-
-#סוגי משפטים
 def p_statement(p):
-    '''statement : expression_statement
-                 | function_declaration
-                 | print_statement
-                 | if_statement
-                 | for_loop
-                 | while_loop
-                 | continue_statement
-                 | break_statement
-                 | return_statement
-                '''
+    '''
+    statement : assignment
+              | print_statement
+              | if_statement
+              | while_statement
+              | for_statement
+              | NEWLINE
+    '''
     p[0] = p[1]
-# | variable_declaration
 
-# Parsing rules for reserved keywords
-def p_function_declaration(p):
-    'function_declaration : DEF IDENTIFIER LEFT_PAREN parameter_list RIGHT_PAREN COLON statement'
-    p[0] = FunctionDeclarationNode(p[2], p[4], p[7])
+def p_while_statement(p):
+    '''
+    while_statement : WHILE expression COLON statements
+    '''
+    p[0] = While(p[2], p[4])
+
+def p_for_statement(p):
+    '''
+    for_statement : FOR VARIABLE IN expression COLON statements
+    '''
+    p[0] = For(Var(p[2]), p[4], p[6])
 
 def p_print_statement(p):
-    'print_statement : PRINT LEFT_PAREN expression_statement RIGHT_PAREN '
-    p[0] = PrintStatementNode(p[2])
+    '''
+    print_statement : PRINT LEFT_PAREN expression RIGHT_PAREN
+    '''
+    p[0] = Print(p[3])
+
+def p_assignment(p):
+    '''
+    assignment : VARIABLE EQUAL expression
+               | VARIABLE EQUAL STRING
+               | VARIABLE EQUAL NUMBER
+    '''
+    if isinstance(p[3], (int, float)):
+        p[0] = VarAssign(p[1], Num(p[3]))
+    elif isinstance(p[3], str) and p[3].isdigit():
+        p[0] = VarAssign(p[1], Num(int(p[3])))
+    elif isinstance(p[3], str):
+        p[0] = VarAssign(p[1], Str(p[3]))
+    else:
+        p[0] = VarAssign(p[1], p[3])
+
+def p_expression(p):
+    '''
+    expression : term
+               | expression PLUS term
+               | expression MINUS term
+               | expression STAR term
+               | expression SLASH term
+               | expression GREATER term
+               | expression LESS term
+               | expression EQUAL_EQUAL term
+               | expression NOT_EQUAL term
+               | expression GREATER_EQUAL term
+               | expression LESS_EQUAL term
+    '''
+    if len(p) == 2:
+        p[0] = p[1]
+    else:
+        p[0] = BinOp(p[1], p[2], p[3])
+
+def p_term(p):
+    '''
+    term : NUMBER
+         | STRING
+         | VARIABLE
+         | FLOAT
+         | list
+    '''
+    if p.slice[1].type == 'NUMBER' or p.slice[1].type == 'FLOAT':
+        p[0] = Num(p[1])
+    elif p.slice[1].type == 'STRING':
+        p[0] = Str(p[1])
+    elif p.slice[1].type == 'VARIABLE':
+        p[0] = Var(p[1])
+    elif p.slice[1].type == 'list':
+        p[0] = p[1]
+    else:
+        p[0] = p[1]
+
+def p_list(p):
+    '''
+    list : LEFT_BRACKET elements RIGHT_BRACKET
+    '''
+    p[0] = List(p[2])
+
+def p_elements(p):
+    '''
+    elements : expression
+             | elements COMMA expression
+    '''
+    if len(p) == 2:
+        p[0] = [p[1]]
+    else:
+        p[0] = p[1] + [p[3]]
 
 def p_if_statement(p):
-    'if_statement : IF expression COLON statement ELSE COLON statement'
-    p[0] = IfStatementNode(p[2], p[4], p[7])
+    '''
+    if_statement : IF expression COLON statements else_statement
+    '''
+    p[0] = If(p[2], p[4], p[5])
 
-def p_for_loop(p):
-    'for_loop : FOR IDENTIFIER IN expression_statement COLON statement'
-    p[0] = ForLoopNode(p[2], p[4], p[6])
-
-def p_while_loop(p):
-    'while_loop : WHILE expression COLON statement'
-    p[0] = WhileLoopNode(p[2], p[4])
-
-def p_continue_statement(p):
-    'continue_statement : CONTINUE'
-    p[0] = ContinueStatementNode()
-
-def p_break_statement(p):
-    'break_statement : BREAK'
-    p[0] = BreakStatementNode()
-
-def p_return_statement(p):
-    'return_statement : RETURN expression'
-    p[0] = ReturnStatementNode(p[2])
-
-
-# מגדיר את רשימת הפרמטרים שמועברים לפונקציה
-def p_parameter_list(p):
-    '''parameter_list : parameter_list
-                      | parameter_list COMMA parameter
-                      | NONE'''
+def p_else_statement(p):
+    '''
+    else_statement :
+                  | ELSE COLON statements
+    '''
     if len(p) == 4:
-        p[0]=[p[1]] + p[3]
-    elif len(p) == 2:
-        if p[1] is None:
-            p[0]=[]
-        else:
-            p[0]=[p[1]]
-
-def p_parameter_list_single(p):
-    'parameter_list : IDENTIFIER'
-    p[0] = [p[1]]
-
-
-#אופרטורים וסימנים
-def p_expression(p):
-    '''expression : expression PLUS expression
-                  | expression MINUS expression
-                  | expression STAR expression
-                  | expression SLASH expression
-
-
-
-                  | NOT expression
-                  | expression BANG_EQUAL expression
-                  | expression EQUAL_EQUAL expression
-                  | expression GREATER expression
-                  | expression GREATER_EQUAL expression
-                  | expression LESS expression
-                  | expression LESS_EQUAL expression
-                  | expression ARROW expression
-                  | LEFT_PAREN expression RIGHT_PAREN
-                  | LEFT_BRACE expression RIGHT_BRACE
-                  | LEFT_BRACKET expression RIGHT_BRACKET
-                  | expression COMMA expression
-                  | expression DOT expression
-                  | expression QUESTION expression
-                  | expression COLON expression'''
-
-# | expression MODULO expression
-# | expression LEFT_SHIFT expression
-# | expression RIGHT_SHIFT expression
-# | primary_expression
-
-def p_bitwise_or_expression(p):
-    '''bitwise_or_expression : bitwise_xor_expression
-                              | bitwise_or_expression '|' bitwise_xor_expression'''
-    if len(p) == 2:
-        p[0] = p[1]
+        p[0] = Else(p[3])
     else:
-        p[0] = BinaryOperationNode(p[2], p[1], p[3])
-
-
-def p_bitwise_xor_expression(p):
-    '''bitwise_xor_expression : bitwise_and_expression
-                               | bitwise_xor_expression '^' bitwise_and_expression'''
-    if len(p) == 2:
-        p[0] = p[1]
-    else:
-        p[0] = BinaryOperationNode(p[2], p[1], p[3])
-
-
-def p_bitwise_and_expression(p):
-    '''bitwise_and_expression : comparison_expression
-                               | bitwise_and_expression '&' comparison_expression'''
-    if len(p) == 2:
-        p[0] = p[1]
-    else:
-        p[0] = BinaryOperationNode(p[2], p[1], p[3])
-
-
-# def p_comparison_expression(p):
-#     '''comparison_expression : shift_expression
-#                               | comparison_expression comp_operator shift_expression'''
-#     if len(p) == 2:
-#         p[0] = p[1]
-#     else:
-#         p[0] = BinaryOperationNode(p[2], p[1], p[3])
-#
-
-
-# def p_shift_expression(p):
-#     '''shift_expression : additive_expression
-#                         | shift_expression LEFT_SHIFT additive_expression
-#                         | shift_expression RIGHT_SHIFT additive_expression'''
-#     if len(p) == 2:
-#         p[0] = p[1]
-#     else:
-#         p[0] = BinaryOperationNode(p[2], p[1], p[3])
-#
-#
-# def p_additive_expression(p):
-#     '''additive_expression : multiplicative_expression
-#                            | additive_expression PLUS multiplicative_expression
-#                            | additive_expression MINUS multiplicative_expression'''
-#     if len(p) == 2:
-#         p[0] = p[1]
-#     else:
-#         p[0] = BinaryOperationNode(p[2], p[1], p[3])
-
-
-def p_multiplicative_expression(p):
-    '''multiplicative_expression : unary_expression
-                                  | multiplicative_expression TIMES unary_expression
-                                  | multiplicative_expression DIVIDE unary_expression
-                                  | multiplicative_expression MODULO unary_expression'''
-    if len(p) == 2:
-        p[0] = p[1]
-    else:
-        p[0] = BinaryOperationNode(p[2], p[1], p[3])
+        p[0] = None
 
 def p_error(p):
-    print("Syntax error in input!")
+    if p:
+        print(f"Syntax error at token '{p.value}'")
+    else:
+        print("Syntax error at EOF")
 
+def p_variable_assignment(p):
+    '''
+    variable_assignment : VARIABLE EQUAL expression
+    '''
+    p[0] = VarAssign(p[1], p[3])
 
-# Build the parser
+def p_variable(p):
+    '''
+    variable : VARIABLE
+    '''
+    p[0] = Var(p[1])
+
+def p_expression_var(p):
+    '''
+    expression : variable
+    '''
+    p[0] = p[1]
+
 parser = yacc.yacc()
 
-# Test the parser with an example input
-input_code = '''
-print(1 + 2 * 3)
-'''
-parser.parse(input_code)
+# # דוגמאות קוד לבדיקה
+# input_code1 = '''
+# הדפס("----------------")
+# הדפס("שלום עולם!!")
+# '''
+# input_code2 = '''
+# נעמה = 30-20
+# מיכל = 2*5
+# הדפס("----------------")
+# הדפס(נעמה*מיכל)
+# '''
+# input_code3 = '''
+# נעמה=10*10
+# מיכל=200/2
+#
+# אם נעמה + מיכל >=100 :
+# הדפס("----------------")
+#     הדפס ("נעמה ומיכל")
+# אחרת:
+# הדפס("----------------")
+#     הדפס ("קטנטת מ100")
+# '''
+# input_code4 = '''
+# נעמה = 1
+# בעוד נעמה < 50 :
+# הדפס(נעמה)
+# נעמה = נעמה + 1
+# '''
+#
+# input_code5 = '''
+# עבור משתנה ב [1, 2, 3, 4, 5]:
+#     הדפס( משתנה)
+# '''
