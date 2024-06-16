6bb3c2a9b6cb2e1d5ac5748aa089a42b2bd8ab21
N <166038881+naama-80820@users.noreply.github.com>
2024-06-16 04:15:43 +0300

    Update gen.py

--- codegen/gen.py
+++ codegen/gen.py
@@ -1,69 +1,38 @@
-# codegen.py
-
-from ast import *
+from semantic.my_ast import Num, BinOp, Print, Str, VarAssign, Var, If, Else, While, For,List
 
 def generate_code(node):
-    if isinstance(node, ProgramNode):
-        # Generate code for each statement in the program
-        code = ""
-        for statement in node.statements:
-            code += generate_code(statement) + "\n"
-        return code
-    elif isinstance(node, VariableDeclarationNode):
-        return f"{node.identifier} = {generate_code(node.expression)}"
-    elif isinstance(node, FunctionDeclarationNode):
-        parameters = ", ".join(node.parameters)
-        body_code = generate_code(node.body)
-        return f"def {node.identifier}({parameters}):\n{indent(body_code)}"
-    elif isinstance(node, BinaryOperationNode):
-        left = generate_code(node.left)
-        right = generate_code(node.right)
-        operator = {
-            TokenType.PLUS: '+',
-            TokenType.MINUS: '-',
-            TokenType.STAR: '*',
-            TokenType.SLASH: '/',
-            TokenType.EQUAL_EQUAL: '==',
-            # Add more operators as needed
-        }.get(node.operator)
-        return f"({left} {operator} {right})"
-    elif isinstance(node, NumberNode):
+    if isinstance(node, Num):
         return str(node.value)
-    elif isinstance(node, IdentifierNode):
+    elif isinstance(node, Str):
+        return f'"{node.value}"'
+    elif isinstance(node, Var):
         return node.name
-    elif isinstance(node, PrintStatementNode):
-        expression_code = generate_code(node.expression)
-        return f"print({expression_code})"
-    elif isinstance(node, BlockNode):
-        # Generate code for a block of statements
-        code = ""
-        for statement in node.statements:
-            code += generate_code(statement) + "\n"
+    elif isinstance(node, VarAssign):
+        return f"{node.name} = {generate_code(node.value)}"
+    elif isinstance(node, BinOp):
+        return f"{generate_code(node.left)} {node.op} {generate_code(node.right)}"
+    elif isinstance(node, Print):
+        return f"print({generate_code(node.value)})"
+    elif isinstance(node, If):
+        code = f"if {generate_code(node.condition)}:\n"
+        for stmt in node.then_body:
+            code += f"    {generate_code(stmt)}\n"
+        if node.else_body:
+            code += "else:\n"
+            for stmt in node.else_body.body:
+                code += f"    {generate_code(stmt)}\n"
         return code
-    elif isinstance(node, AssignmentNode):
-        return f"{node.identifier} = {generate_code(node.expression)}"
-    elif isinstance(node, UnaryOperationNode):
-        operand_code = generate_code(node.operand)
-        operator = {
-            TokenType.MINUS: '-',
-            TokenType.PLUS: '+',
-            # Add more unary operators as needed
-        }.get(node.operator)
-        return f"{operator}{operand_code}"
-    elif isinstance(node, FunctionCallNode):
-        arguments_code = ", ".join(generate_code(arg) for arg in node.arguments)
-        return f"{node.name}({arguments_code})"
-    else:
-        raise RuntimeError(f"Unknown AST node type: {type(node)}")
+    elif isinstance(node, While):
+        code = f"while {generate_code(node.condition)}:\n"
+        for stmt in node.body:
+            code += f"    {generate_code(stmt)}\n"
+        return code
+    elif isinstance(node, For):
+        code = f"for {generate_code(node.variable)} in {generate_code(node.iterable)}:\n"
+        for stmt in node.body:
+            code += f"    {generate_code(stmt)}\n"
+        return code
+    elif isinstance(node, List):
+        return f"[{', '.join(generate_code(element) for element in node.elements)}]"
+    return ""
 
-def indent(code, num_spaces=4):
-    lines = code.split('\n')
-    indented_code = ""
-    for line in lines:
-        indented_code += " " * num_spaces + line + "\n"
-    return indented_code.strip()
-
-# Example usage:
-# ast = ...  # Your AST instance
-# python_code = generate_code(ast)
-# print(python_code)
